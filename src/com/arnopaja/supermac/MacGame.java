70d76ee8a264db1dde0def98489469ac382a6078
Ari Weiland <math1man@gmail.com>
2014-02-19 12:52:53 -0600

    Starting to figure things out

--- src/com/arnopaja/supermac/MacGame.java
+++ src/com/arnopaja/supermac/MacGame.java
@@ -1,5 +1,6 @@
 package com.arnopaja.supermac;
 
+import com.arnopaja.supermac.screen.GameScreen;
 import com.badlogic.gdx.Game;
 
 public class MacGame extends Game {
@@ -7,5 +8,6 @@ public class MacGame extends Game {
     @Override
     public void create() {
         System.out.println("ZBGame Created!");
+        setScreen(new GameScreen());
     }
 }
---
+++ src/com/arnopaja/supermac/World/GameWorld.java
@@ -0,0 +1,10 @@
+package com.arnopaja.supermac.world;
+
+/**
+ * @author Ari Weiland
+ */
+public class GameWorld {
+    public void update(float delta) {
+        System.out.println("GameWorld - update");
+    }
+}
