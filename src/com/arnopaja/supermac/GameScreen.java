c1a04ad238a38381b5286032215b861d9e86c29b
Ari Weiland <math1man@gmail.com>
2014-02-19 12:01:04 -0600

    Starting to figure things out

---
+++ src/com/arnopaja/supermac/GameScreen.java
@@ -0,0 +1,43 @@
+package com.arnopaja.supermac;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * @author Ari Weiland
+ */
+public class GameScreen implements Screen {
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i2) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
