55196424e7b457eca37ef9a4e651ac8ad9e20836
Ari Weiland <math1man@gmail.com>
2014-02-26 12:48:27 -0600

    Working on dealing with tiles

--- src/com/arnopaja/supermac/objects/Tile.java
+++ src/com/arnopaja/supermac/objects/Tile.java
@@ -10,32 +10,59 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  */
 public class Tile extends GridElement {
 
+    public enum TileType {
+        BUILDING("b", true, AssetLoader.buildingTile, false),
+        GROUND("g", true, AssetLoader.groundTile, true),
+        PATH("p", true, AssetLoader.pathTile, true),
+        NULL("n", false, null, false);
+
+        public final String tileCode;
+        public final boolean isRendered;
+        public final TextureRegion sprite; // TODO: resolve this error
+        public final boolean isPathable;
+
+        private TileType(String tileCode, boolean isRendered, TextureRegion sprite, boolean isPathable) {
+            this.tileCode = tileCode;
+            this.isRendered = isRendered;
+            this.sprite = sprite;
+            this.isPathable = isPathable;
+        }
+
+        public static TileType getTileType(String tileCode) {
+            for (TileType tt : TileType.values()) {
+                if (tt.tileCode.equals(tileCode)) {
+                    return tt;
+                }
+            }
+            return NULL;
+        }
+    }
+
+    private TileType type;
     private TextureRegion sprite;
     private boolean isPathable;
 
-    private Tile(boolean isRendered, TextureRegion sprite, boolean isPathable) {
+    private Tile(TileType type, boolean isRendered, TextureRegion sprite, boolean isPathable) {
         super(isRendered, false);
+        this.type = type;
         this.sprite = sprite;
         this.isPathable = isPathable;
     }
 
     public static Tile createNullTile() {
-        return new Tile(false, null, false);
+        return createTile(TileType.NULL);
     }
 
     public static Tile createTile(TextureRegion sprite, boolean isPathable) {
-        return new Tile(true, sprite, isPathable);
+        return new Tile(null, true, sprite, isPathable);
     }
 
     public static Tile createTile(String tileCode) {
-        if (tileCode.equals("b"))
-            return createTile(AssetLoader.buildingTile, false);
-        else if (tileCode.equals("g"))
-            return createTile(AssetLoader.groundTile, true);
-        else if (tileCode.equals("p"))
-            return createTile(AssetLoader.pathTile, true);
-        else
-            return createNullTile();
+        return createTile(TileType.getTileType(tileCode));
+    }
+
+    private static Tile createTile(TileType tileType) {
+        return new Tile(tileType, tileType.isRendered, tileType.sprite, tileType.isPathable);
     }
 
     @Override
@@ -44,12 +71,23 @@ public class Tile extends GridElement {
         return true;
     }
 
+    public TileType getType() {
+        return type;
+    }
+
     public TextureRegion getSprite() {
         return sprite;
     }
 
     public boolean isPathable() {
         return isPathable;
+    }
+
+    public void setType(TileType type) {
+        this.type = type;
+        isRendered = type.isRendered;
+        sprite = type.sprite;
+        isPathable = type.isPathable;
     }
 
     public void setSprite(TextureRegion sprite) {
