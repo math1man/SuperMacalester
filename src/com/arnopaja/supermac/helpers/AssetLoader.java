27e10710b4a73ff51b222a8633f8acd7f50d0822
Ari Weiland <math1man@gmail.com>
2014-03-27 20:53:02 -0500

    Added Chest

--- src/com/arnopaja/supermac/helpers/AssetLoader.java
+++ src/com/arnopaja/supermac/helpers/AssetLoader.java
@@ -52,6 +52,10 @@ public class AssetLoader {
 //        texture = new Texture(Gdx.files.internal("data/texture.png"));
 //        texture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
 
+        //--------------------------//
+        //          Tiles           //
+        //--------------------------//
+
         // TODO: build a basic texture file and set up tiles
         texture = new Texture(Gdx.files.internal("data/landscapetiles/darkgrasstile1.png"));
         grass0 = new TextureRegion(texture, 0, 0, 32, 32);
@@ -118,7 +122,22 @@ public class AssetLoader {
 
         MapLoader.initTileMap(); // Must be called after all tiles are loaded
 
+        //--------------------------//
+        //         Entities         //
+        //--------------------------//
+
+        texture = new Texture(Gdx.files.internal("data/landscapetiles/chest_closed.png"));
+        chestClosed = new TextureRegion(texture, 0, 0, 512, 128);
+        chestClosed.flip(false, true);
+        texture = new Texture(Gdx.files.internal("data/landscapetiles/chest_open.png"));
+        chestOpen = new TextureRegion(texture, 0, 0, 512, 128);
+        chestOpen.flip(false, true);
+
         loadCharacters(0, 0);
+
+        //--------------------------//
+        //          Other           //
+        //--------------------------//
 
         font = new BitmapFont(Gdx.files.internal("data/text.fnt"));
         shadow = new BitmapFont(Gdx.files.internal("data/shadow.fnt"));
