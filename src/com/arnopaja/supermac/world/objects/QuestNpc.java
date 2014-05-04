0473b6e5b362174dbee2cf974107ec3720bb887b
Ari Weiland <math1man@gmail.com>
2014-05-04 14:08:54 -0500

    Updated the way Goals work: Goals now hold a List of QuestNpcs, each of which has its own interaction and its own delay parameter QuestNpcs also now have an isPrimary boolean which determines whether this entity can advance the quest, or whether it is an auxiliary entity. Updated the parsers for everything accordingly Removed EntityInteraface, QuestEntity, and NonRenderedQuestEntity as these seemed irrelevant.  The QuestNpc now provides a secondary constructor that creates a non-rendered QuestNpc Updated the plot_temp.txt file too

--- src/com/arnopaja/supermac/world/objects/QuestNpc.java
+++ src/com/arnopaja/supermac/world/objects/QuestNpc.java
@@ -2,7 +2,7 @@ package com.arnopaja.supermac.world.objects;
 
 import com.arnopaja.supermac.helpers.Interaction;
 import com.arnopaja.supermac.helpers.load.SuperParser;
-import com.arnopaja.supermac.plot.QuestEntity;
+import com.arnopaja.supermac.plot.Quest;
 import com.arnopaja.supermac.world.grid.Direction;
 import com.arnopaja.supermac.world.grid.Location;
 import com.google.gson.JsonElement;
@@ -11,23 +11,47 @@ import com.google.gson.JsonObject;
 /**
  * @author Ari Weiland
  */
-public class QuestNpc extends MapNpc implements QuestEntity {
+public class QuestNpc extends MapNpc {
 
     private final Location location;
+    private final boolean isPrimary;
+    private final boolean delay;
 
-    public QuestNpc(String name, Location location, Direction direction) {
-        super(name, null, direction, true, Interaction.NULL);
-        this.location = location;
+    /**
+     * Constructs a non-rendered QuestNpc
+     * @param location
+     * @param interaction
+     * @param isPrimary
+     * @param delay
+     */
+    public QuestNpc(Location location, Interaction interaction, boolean isPrimary, boolean delay) {
+        this(null, location, Direction.WEST, interaction, isPrimary, delay);
     }
 
-    @Override
-    public void activate(Interaction interaction) {
-        this.interaction = interaction;
+    /**
+     * Constructs a rendered QuestNpc
+     * @param name
+     * @param location
+     * @param direction
+     * @param interaction
+     * @param isPrimary
+     * @param delay
+     */
+    public QuestNpc(String name, Location location, Direction direction, Interaction interaction, boolean isPrimary, boolean delay) {
+        super(name, null, direction, true, interaction);
+        this.location = location;
+        this.isPrimary = isPrimary;
+        this.delay = delay;
+    }
+
+    public void activate(Quest quest) {
+        if (isPrimary) {
+            interaction = interaction.attach(quest);
+        }
         forceChangeGrid(location);
     }
 
-    @Override
-    public void deactivate(boolean delay) {
+    public void deactivate() {
         setInteractable(false);
         removeFromGrid(delay);
     }
@@ -40,23 +64,35 @@ public class QuestNpc extends MapNpc implements QuestEntity {
             if (object.has("name")) {
                 name = getString(object, "name");
             }
-            Location location = null;
-            if (has(object, Location.class)) {
-                location = getObject(object, Location.class);
-            }
+            Location location = getObject(object, Location.class);
             Direction direction = Direction.WEST;
             if (has(object, Direction.class)) {
                 direction = getObject(object, Direction.class);
             }
-            return new QuestNpc(name, location, direction);
+            Interaction interaction = Interaction.NULL;
+            if (has(object, Interaction.class)) {
+                interaction = getObject(object, Interaction.class);
+            }
+            boolean isPrimary = true;
+            if (object.has("primary")) {
+                isPrimary = getBoolean(object, "primary");
+            }
+            boolean delay = true;
+            if (object.has("delay")) {
+                delay = getBoolean(object, "delay");
+            }
+            return new QuestNpc(name, location, direction, interaction, isPrimary, delay);
         }
 
         @Override
         public JsonElement toJson(QuestNpc object) {
             JsonObject json = new JsonObject();
             addString(json, "name", object.getName());
-            addObject(json, object.getLocation(), Location.class);
+            addObject(json, object.location, Location.class);
             addObject(json, object.getDirection(), Direction.class);
+            addObject(json, object.interaction, Interaction.class);
+            addBoolean(json, "primary", object.isPrimary);
+            addBoolean(json, "delay", object.delay);
             return json;
         }
     }
