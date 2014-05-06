5e642da2c6b11a6f8fbcfcdfd3b7f04e130a13ac
Ari Weiland <math1man@gmail.com>
2014-05-06 03:31:27 -0500

    Merge remote-tracking branch 'origin/battle' into battle
    Conflicts:
    	src/com/arnopaja/supermac/battle/Battle.java

--- src/com/arnopaja/supermac/battle/Battle.java
+++ src/com/arnopaja/supermac/battle/Battle.java
@@ -5,6 +5,7 @@ import com.arnopaja.supermac.battle.characters.*;
 import com.arnopaja.supermac.helpers.Controller;
 import com.arnopaja.supermac.helpers.dialogue.DialogueOptions;
 import com.arnopaja.supermac.helpers.dialogue.DialogueStyle;
+import com.arnopaja.supermac.helpers.dialogue.DialogueText;
 import com.arnopaja.supermac.helpers.interaction.Interaction;
 import com.arnopaja.supermac.helpers.interaction.Interactions;
 import com.arnopaja.supermac.helpers.interaction.MultiInteraction;
